0249dba6b560ffc2613d56b52764ad21cb15cf54
Yishu See <yishuyishu@gmail.com>
2025-10-18 13:39:35 +0800

    Add system prompt

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import { argv } from "node:process";
 import { readFile, stat } from "node:fs/promises";
+import path from "node:path";
+import { argv } from "node:process";
 
 import OpenAI from "openai";
 
 import type { ConversationItem } from "openai/resources/conversations";
 
 interface Workflow {
+  systemPrompt?: string;
   steps: string[];
 }
 
@@ -13,7 +15,17 @@ const runWorkflow = async (workflow: Workflow) => {
   const client = new OpenAI();
   const { steps } = workflow;
 
-  const conversation = await client.conversations.create();
+  const initialItems = [
+    {
+      type: "message",
+      role: "system",
+      content: workflow.systemPrompt || "You are a helpful assistant.",
+    },
+  ];
+
+  const conversation = await client.conversations.create({
+    items: initialItems,
+  });
 
   for (const step of steps) {
     await client.responses.create({
@@ -46,18 +58,26 @@ const fileStatAtPath = async (path: string) => {
   }
 };
 
-const readFileAtPath = async (filePath: string) => {
+const runWorkflowAtPath = async (filePath: string) => {
+  let workflow: Workflow = { steps: [] };
   try {
     const data = await readFile(filePath, { encoding: "utf8" });
-    const workflow = JSON.parse(data) as Workflow;
-    runWorkflow(workflow);
-  } catch (err) {
+    workflow = JSON.parse(data) as Workflow;
+
+    const directory = path.dirname(filePath);
+    const workflowPrompt = await readFile(`${directory}/workflow.md`);
+    runWorkflow({ ...workflow, systemPrompt: workflowPrompt.toString() });
+  } catch (err: any) {
+    if (err.code === "ENOENT" && err.path.endsWith("workflow.md")) {
+      runWorkflow(workflow);
+      return;
+    }
     console.error("Error reading file:", err);
   }
 };
 
 const printAssistantLastMessage = (items: ConversationItem[]) => {
-  items.forEach((item) => {
+  for (const item of items) {
     if (item.type === "message") {
       if (item.role !== "assistant") {
         return;
@@ -76,9 +96,9 @@ const main = async () => {
   const workflowPath = argv[2];
   const res = await fileStatAtPath(workflowPath);
   if (res?.isDirectory) {
-    readFileAtPath(`${workflowPath}/workflow.json`);
+    runWorkflowAtPath(`${workflowPath}/workflow.json`);
   } else if (res?.isFile) {
-    readFileAtPath(workflowPath);
+    runWorkflowAtPath(workflowPath);
   } else {
     console.log("Please pass a file or directory path.");
   }
