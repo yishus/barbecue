f4ecfee4e808114039ebced8af12d745f24ebd16
Yishu See <yishuyishu@gmail.com>
2025-11-04 16:24:34 +0800

    Add support to read from directory for readFile tool

--- tools/readFile.ts
+++ tools/readFile.ts
@@ -1,11 +1,14 @@
-import { readFile } from "node:fs/promises";
+import { readdir, readFile } from "node:fs/promises";
 
 import type { Tool } from "openai/resources/responses/responses";
+
+import { fileStatAtPath } from "../helper";
 
 export const definition: Tool = {
   type: "function",
   name: "read_file",
-  description: "Reads the content of a file at the specified path.",
+  description:
+    "Reads the content of a file at the specified path. If the path is a directory, list the contents.",
   parameters: {
     type: "object",
     properties: {
@@ -24,11 +27,19 @@ export const callFunction = async (args: { path: string }) => {
   const { path } = args;
 
   try {
-    const data = await readFile(path, { encoding: "utf8" });
-
-    return data.toString();
+    const res = await fileStatAtPath(path);
+    if (res?.isDirectory) {
+      const files = await readdir(path);
+      return `Contents of directory "${path}":\n` + files.join("\n");
+    } else if (res?.isFile) {
+      const data = await readFile(path, { encoding: "utf8" });
+      return data.toString();
+    } else {
+      return `The path "${path}" does not exist.`;
+    }
   } catch (err: any) {
     console.error("Error reading file:", err);
+    return `Error reading file at path "${path}": ${err.message}`;
   }
 };
 
